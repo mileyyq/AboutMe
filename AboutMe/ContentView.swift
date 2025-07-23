0082d91d80fc09404131db6ef8b5ca1b83ec8f81
Miley <cutegoldendragon@gmail.com>
2025-07-23 14:59:22 -0400

    fixed fun facts and added to interests (+ spacing)

--- AboutMe/ContentView.swift
+++ AboutMe/ContentView.swift
@@ -24,7 +24,7 @@ struct ContentView: View {
                     .fontWeight(.semibold)
                     .foregroundColor(Color(hue: 0.043, saturation: 0.147, brightness: 0.843))
                     .multilineTextAlignment(.center)
-                    .padding(EdgeInsets(top: 10, leading: 5, bottom: 20, trailing: 10))
+                    .padding(EdgeInsets(top: 10, leading: 0, bottom: 20, trailing: 10))
                     .padding(.horizontal)
                 Text("13 🇻🇳 |  ISFP  |  ♡ musicophile")
                     .fontWeight(.bold)
@@ -37,11 +37,11 @@ struct ContentView: View {
             Text("Interests !! :")
                 .font(.system(size: 27.5))
                 .foregroundColor(Color(hue: 0.047, saturation: 0.124, brightness: 0.572))
-                .padding(EdgeInsets(top: 25, leading: 5, bottom: 15, trailing: 5))
-            Text("music, badminton, my stuffed animals, friends, video games, drawing ...more")
+                .padding(EdgeInsets(top: 10, leading: 5, bottom: 15, trailing: 5))
+            Text("music, badminton, my stuffed animals, friends, video games, drawing, tv shows ...more")
                 .fontWeight(.bold)
                 .foregroundColor(Color(hue: 0.047, saturation: 0.124, brightness: 0.572))
-                .padding(EdgeInsets(top: 0, leading: 25, bottom: 20, trailing: 25))
+                .padding(EdgeInsets(top: 0, leading: 15, bottom: 20, trailing: 15))
         }
         HStack{
             Image("badminton")
@@ -59,12 +59,16 @@ struct ContentView: View {
             Text("Fun Facts About Me !! :")
                 .font(.system(size: 27.5))
                 .foregroundColor(Color(hue: 0.047, saturation: 0.124, brightness: 0.572))
-                .padding(EdgeInsets(top: 25, leading: 5, bottom: 15, trailing: 5))
+                .padding(EdgeInsets(top: 0, leading: 5, bottom: 15, trailing: 5))
             Text("- i've broken a collarbone before")
                 .fontWeight(.bold)
                 .foregroundColor(Color(hue: 0.047, saturation: 0.124, brightness: 0.572))
              
             Text("- i used to play tennis and do running")
+                .fontWeight(.bold)
+                .foregroundColor(Color(hue: 0.047, saturation: 0.124, brightness: 0.572))
+            
+            Text("- my favorite color is white")
                 .fontWeight(.bold)
                 .foregroundColor(Color(hue: 0.047, saturation: 0.124, brightness: 0.572))
                
@@ -77,7 +81,7 @@ struct ContentView: View {
                         .fontWeight(.bold)
                         .foregroundColor(Color(red: 0.843, green: 0.75, blue: 0.717))
                         .multilineTextAlignment(.center)
-                        .padding(EdgeInsets(top: 10, leading: 5, bottom: 0, trailing: 5))
+                        .padding(EdgeInsets(top: 25, leading: 5, bottom: 0, trailing: 5))
 
     }
 }
